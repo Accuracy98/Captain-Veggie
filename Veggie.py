35a12011b06b8af209c003c21f9651be213e55b0
Accuracy98 <y1500846103outlook.com>
2023-11-17 16:42:51 -0500

    fix

--- Veggie.py
+++ Veggie.py
@@ -1,22 +1,38 @@
 from FieldInhabitant import FieldInhabitant
 
+
 class Veggie(FieldInhabitant):
-    def __init__(self, symbol: str, name, points) -> None:
-        self._name = name
-        self._points = points
+    """A class representing a vegetable in a field.
+
+    Attributes:
+        symbol (str): The symbol representing the vegetable.
+        name (str): The name of the vegetable.
+        points (int): The points associated with the vegetable.
+
+    Methods:
+        getName(): Get the name of the vegetable.
+        setName(name: str): Set the name of the vegetable.
+        getPoints(): Get the points associated with the vegetable.
+        setPoints(points: int): Set the points associated with the vegetable.
+        __str__(): Get a string representation of the vegetable.
+
+    """
+    def __init__(self, symbol: str, name: str, points: int) -> None:
+        self.__name = name
+        self.__points = points
         super().__init__(symbol)
-    def getName(self):
-        return self._name
 
-    def setName(self, name):
-        self._name = name
+    def getName(self) -> str:
+        return self.__name
 
-    def getPoints(self):
-        return self._points
+    def setName(self, name: str) -> None:
+        self.__name = name
 
-    def setPoints(self, points):
-        self._points = points
+    def getPoints(self) -> int:
+        return self.__points
 
-    def  __str__(self) -> str:
-        return super().__str__()
-    
+    def setPoints(self, points: int) -> None:
+        self.__points = points
+
+    def __str__(self) -> str:
+        return f"{self.__name} {self.__points} points"
