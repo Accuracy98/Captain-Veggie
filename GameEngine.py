d471371c6329bacbdc6617cbe398a73d0536551e
Accuracy98 <y1500846103outlook.com>
2023-11-20 15:56:30 -0500

    Finished movement functions but got many problems about "not a known member of "None""

--- GameEngine.py
+++ GameEngine.py
@@ -1,7 +1,7 @@
-
 import csv
 import random
 from Captain import Captain
+from Creature import Creature
 from Veggie import Veggie
 from FieldInhabitant import FieldInhabitant
 from Rabbit import Rabbit
@@ -14,7 +14,7 @@ class GameEngine:
         self.__HIGHSCOREFILE = "highscore.data"
         self.__field: list[list[FieldInhabitant | None]] = []
         self.__rabbits: list[Rabbit] = []
-        self.__caption: Captain | None = None
+        self.__captain: Captain | None = None
         self.__vegetables: list[Veggie] = []
         self.__score = 0
 
@@ -57,7 +57,7 @@ class GameEngine:
             while True:
                 x, y = random.randint(0, width - 1), random.randint(0, height - 1)
                 if self.__field[x][y] is None:
-                    rabbit = Rabbit(x,y)
+                    rabbit = Rabbit(x, y)
                     self.__rabbits.append(rabbit)
                     self.__field[x][y] = rabbit
                     break
@@ -80,16 +80,96 @@ class GameEngine:
         ...
 
     def moveRabbits(self):
-        ...
+        height = len(self.__field)
+        width = len(self.__field[0])
+        derections = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (0, -1),
+            (0, 0),
+            (1, 1),
+            (1, 0),
+            (1, -1),
+        ]
+        for rabbit in self.__rabbits:
+            x = rabbit.getX()
+            y = rabbit.getY()
+            dx,dy = random.choice(derections)
+            new_x, new_y = x + dx, y + dy
+
+            if 0 <= new_x < width and 0 <= new_y < height:
+                if self.__field[new_x][new_y] is None or isinstance(self.__field[new_x][new_y], Veggie):
+                    self.__field[x][y] = None
+                    rabbit.setX(new_x)
+                    rabbit.setY(new_y)
+                    self.__field[new_x][new_y] = rabbit
+
 
     def moveCptVertical(self, movement):
-        ...
+        height = len(self.__field)
+        new_y = self.__captain.getY() + movement
+        if 0 <= new_y < height:
+            new_position = self.__field[self.__captain.getX()][new_y]
+            if isinstance(new_position, Veggie):
+                print(f"A delicious {new_position.getName()} has been found")
+                self.__captain.addVeggie(new_position)
+                self.__score += 1
+                self.__field[self.__captain.getX()][self.__captain.getY()] = None
+                self.__captain.setY(new_y) 
+                self.__field[self.__captain.getX()][new_y] = self.__captain
+            elif isinstance(new_position, Rabbit):
+                return
+            elif new_position is None:
+                self.__field[self.__captain.getX()][self.__captain.getY()] = None
+                self.__captain.setY(new_y) 
+                self.__field[self.__captain.getX()][new_y] = self.__captain
+
 
     def moveCptHorizontal(self, movement):
-        ...
+        width = len(self.__field[0])
+        new_x = self.__captain.getX() + movement
+        if 0 <= new_x < width:
+            new_position = self.__field[new_x][self.__captain.getY()]
+            if isinstance(new_position, Veggie):
+                print(f"A delicious {new_position.getName()} has been found")
+                self.__captain.addVeggie(new_position)
+                self.__score += 1
+                self.__field[self.__captain.getX()][self.__captain.getY()] = None
+                self.__captain.setX(new_x) 
+                self.__field[new_x][self.__captain.getY()] = self.__captain
+            elif isinstance(new_position, Rabbit):
+                return
+            elif new_position is None:
+                self.__field[self.__captain.getX()][self.__captain.getY()] = None
+                self.__captain.setX(new_x)
+                self.__field[new_x][self.__captain.getY()] = self.__captain
 
     def moveCaptain(self):
-        ...
+        derections = input("Would you like to move up(W), down(S), left(A), or right(D):").lower()
+        if derections == "w" :
+            if self.__captain.getY() > 0:
+                self.moveCptVertical(-1)
+            else:
+                print("Can not move")
+        elif derections =='s':
+            if self.__captain.getY() < len(self.__field) - 1:
+                self.moveCptVertical(1)
+            else:
+                print("Can not move")
+        elif derections == 'a':
+            if self.__captain.getX() > 0:
+                self.moveCptHorizontal(-1)
+            else:
+                print("Can not move")
+        elif derections == 'd':
+            if self.__captain.getX() < len(self.__field[0]) - 1:
+                self.moveCptHorizontal(1)
+            else:
+                print("Can not move")
+        else:
+            print(f"{derections} is not a valid option")
 
     def gameOver(self):
         ...
@@ -103,3 +183,8 @@ if __name__ == "__main__":
     game_engine.initVeggies()
     game_engine.initCaption()
     game_engine.initRabbits()
+    game_engine.moveCaptain()
+    game_engine.moveRabbits()
+    game_engine.moveCptVertical(1)
+    game_engine.moveCptHorizontal(1)
+    game_engine.moveCaptain()
