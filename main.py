a4a6664887c36347b43aac9efc37614c673898f6
Fledge Shiu <xzk0701@gmail.com>
2023-11-22 15:01:50 -0500

    Add main functions.

---
+++ main.py
@@ -0,0 +1,29 @@
+# Author: Zhekang Xu
+# Date: 2023-11-22
+# Description: main functions, the enter of program main functions, the enter of program
+
+from GameEngine import GameEngine
+
+
+def main():
+    game_engine = GameEngine()
+    game_engine.initVeggies()
+    game_engine.initCaptain()
+    game_engine.initRabbits()
+    game_engine.intro()
+    remaining_vegetables = game_engine.remainingVeggies()
+    while remaining_vegetables != 0:
+        print(
+            f"{remaining_vegetables} veggies remaining. Current score: {game_engine.getScore()}"
+        )
+        game_engine.printField()
+        game_engine.moveRabbits()
+        game_engine.moveCaptain()
+        remaining_vegetables = game_engine.remainingVeggies()
+
+    game_engine.gameOver()
+    game_engine.highScore()
+
+
+if __name__ == "__main__":
+    main()
